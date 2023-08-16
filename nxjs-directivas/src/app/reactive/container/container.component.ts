0c10e6359d356683237df080d7fb3dc362f794ad
coderoycc <90710352+coderoycc@users.noreply.github.com>
2023-08-15 21:48:36 -0400

    RxJs 2

---
+++ nxjs-directivas/src/app/reactive/container/container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent {
+
+  myObservable = new Observable<number>();
+}
