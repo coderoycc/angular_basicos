35da2afe6efbdbb8fb49436242ca419618478a13
Roberto Chambi <rcchambi4@umsa.bo>
2023-09-17 22:34:31 -0400

    NUevo forms array y getForm Controll

--- formularios/src/app/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
+++ formularios/src/app/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactive',
@@ -9,10 +9,33 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class FormularioReactiveComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    proyectoR: this.fb.control('Hola', [Validators.required, Validators.minLength(3), Validators.maxLength(5)]),
-    horasR: this.fb.control(0, [Validators.required, Validators.min(0), Validators.max(10)]),
+    proyectoR: this.fb.control('Hola', 
+      [
+        Validators.required, 
+        Validators.minLength(3), 
+        Validators.maxLength(5)
+      ]
+    ),
+    horasR: this.fb.control(0, 
+      [
+        Validators.required, 
+        Validators.min(0), 
+        Validators.max(10)
+      ]
+    ),
     tecnologiasR: this.fb.array([])
   });
+  get tecnologiasR(){
+    return this.miFormulario.get("tecnologiasR") as FormArray
+    // Devolvemos el array de las tecnologias de nuestro formulario
+  }
+
+  // Creamos una variable TECNOLOGIA indivudual
+  tecnologia: FormControl = this.fb.control('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(10),
+  ]) // Para poder activarlo individualmente asignamos como databinding en el campo html [formControl]="tecnologia" 
 
   constructor(private fb: FormBuilder) { }
 
@@ -21,5 +44,12 @@ export class FormularioReactiveComponent implements OnInit {
 
   verificaForm(){
     return this.miFormulario.invalid && this.miFormulario.touched;
+  }
+
+  agregarTecnologia(){
+    if(this.tecnologia.invalid) return;
+    this.tecnologiasR.push(this.fb.control(this.tecnologia.value));
+    this.tecnologia.reset();
+    console.log(this.tecnologiasR.value)
   }
 }
